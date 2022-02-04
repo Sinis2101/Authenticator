95c62450119430c2f9ebff6e590373d1f4a7288c
Sinis2101 <39112178F254DJFsinis2001>
2022-02-04 15:51:56 -0500

    SignUp Form created

--- Models/User.cs
+++ Models/User.cs
@@ -2,9 +2,9 @@
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Username { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
         public string Password { get; set; }
         public DateTime Birthdate { get; set; }
 
