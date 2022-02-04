7202087ba55a4024fa646c91cd02a0ed49508954
Sinis2101 <39112178F254DJFsinis2001>
2022-02-04 13:00:45 -0500

    Created User model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Authenticator.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthdate { get; set; }
+
+    }
+}
